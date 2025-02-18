8d01017cca314a7c2aa0de54671d381833d8aef7
Aysha Thikra <ayshathelegendary@gmail.com>
2025-02-18 08:35:58 +0530

    Created the "DriverSignupServlet.java" file

---
+++ Mega_City_Cabs/src/main/java/Servlet/DriverSignupServlet.java
@@ -0,0 +1,47 @@
+package Servlet;
+
+import Bean.Driver;
+import Dao.DriverDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/driver-signup")
+public class DriverSignupServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private DriverDAO driverDAO = new DriverDAO();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            String userID = driverDAO.generateNextUserID();
+
+            String firstName = request.getParameter("first_name").trim();
+            String lastName = request.getParameter("last_name").trim();
+            String phone = request.getParameter("phone").trim();
+            String email = request.getParameter("email").trim();
+            String address = request.getParameter("address").trim();
+            String nic = request.getParameter("nic").trim();
+            String username = request.getParameter("username").trim();
+            String password = request.getParameter("password").trim();
+            String licenseNumber = request.getParameter("license_number").trim();
+
+            Driver driver = new Driver(userID, firstName, lastName, address, nic, phone, email, username, password, licenseNumber);
+
+            driverDAO.insertDriver(driver);
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher("driver-login.jsp");
+            dispatcher.forward(request, response);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendRedirect("driver-signup.jsp?error=1");
+        }
+    }
+}
