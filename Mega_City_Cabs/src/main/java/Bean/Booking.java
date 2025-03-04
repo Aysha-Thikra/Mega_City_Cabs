991931b89a7f2d06f6c7caad6371c960cd2f66e5
Aysha Thikra <ayshathelegendary@gmail.com>
2025-03-04 16:58:22 +0530

    FEAT: Created Booking bean class

---
+++ Mega_City_Cabs/src/main/java/Bean/Booking.java
@@ -0,0 +1,148 @@
+package Bean;
+
+public class Booking {
+    private String booking_id;
+    private String first_name;
+    private String last_name;
+    private String userID;
+    private String email;
+    private String phone;
+    private String pickup_location;
+    private String drop_location;
+    private String pickup_time;
+    private String route;
+    private String carID;
+    private String car_name;
+    private String estimated_time;
+    private double price_per_minute;
+    private String card_number;
+    private double fare;
+
+    public String getBooking_id() {
+        return booking_id;
+    }
+
+    public void setBooking_id(String booking_id) {
+        this.booking_id = booking_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPickup_location() {
+        return pickup_location;
+    }
+
+    public void setPickup_location(String pickup_location) {
+        this.pickup_location = pickup_location;
+    }
+
+    public String getDrop_location() {
+        return drop_location;
+    }
+
+    public void setDrop_location(String drop_location) {
+        this.drop_location = drop_location;
+    }
+
+    public String getPickup_time() {
+        return pickup_time;
+    }
+
+    public void setPickup_time(String pickup_time) {
+        this.pickup_time = pickup_time;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public String getCarID() {
+        return carID;
+    }
+
+    public void setCarID(String carID) {
+        this.carID = carID;
+    }
+
+    public String getCar_name() {
+        return car_name;
+    }
+
+    public void setCar_name(String car_name) {
+        this.car_name = car_name;
+    }
+
+    public String getEstimated_time() {
+        return estimated_time;
+    }
+
+    public void setEstimated_time(String estimated_time) {
+        this.estimated_time = estimated_time;
+    }
+
+    public double getPrice_per_minute() {
+        return price_per_minute;
+    }
+
+    public void setPrice_per_minute(double price_per_minute) {
+        this.price_per_minute = price_per_minute;
+    }
+    
+    public String getCard_number() {
+        return card_number;
+    }
+
+    public void setCard_number(String card_number) {
+        this.card_number = card_number;
+    }
+
+    public double getFare() {
+        return fare;
+    }
+
+    public void setFare(double fare) {
+        this.fare = fare;
+    }
+}
