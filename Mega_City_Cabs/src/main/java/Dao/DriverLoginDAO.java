b534d7120ad7ee2ca5fc34787205e7ea23c7971d
Aysha Thikra <ayshathelegendary@gmail.com>
2025-03-04 17:01:36 +0530

    FEAT: Create DriverLoginDAO class for database operations

---
+++ Mega_City_Cabs/src/main/java/Dao/DriverLoginDAO.java
@@ -0,0 +1,44 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DriverLoginDAO {
+    private String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/MegaCityCabs_db";
+    private String jdbcUsername = "root";
+    private String jdbcPassword = "1234";
+
+    private static final String CHECK_DRIVER_SQL = "SELECT password FROM users WHERE username = ? AND userLevel = 3";
+
+    protected Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("JDBC Driver not found!");
+        }
+    }
+
+    public String validateDriverLogin(String username, String password) throws SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(CHECK_DRIVER_SQL)) {
+
+            preparedStatement.setString(1, username);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    String storedPassword = resultSet.getString("password");
+                    if (storedPassword.equals(password)) {
+                        return "success";
+                    } else {
+                        return "Incorrect password!";
+                    }
+                } else {
+                    return "Username not found!";
+                }
+            }
+        }
+    }
+}
